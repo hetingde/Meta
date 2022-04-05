aebc940b5b5529d34c49264b2ae202e66b909dad
JaimeZeng <78083448+JaimeZeng@users.noreply.github.com>
2022-04-05 19:24:09 +0800

    ci(utils): Sync with 'extras' bucket

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,9 +1,9 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "Ryanjiena/scoop-apps"
+    [String]$upstream = "Ryanjiena/scoop-apps:master"
 )
 
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
-$dir = "$psscriptroot/../bucket" # checks the parent dir
+$dir = "$PSScriptRoot/../bucket" # checks the parent dir
 Invoke-Expression -command "& '$autopr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
